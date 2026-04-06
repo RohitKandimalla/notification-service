23f65bc05117c3fb3d19b6c6ce2956eb1a631ef4
Edwin Tan <N119494@icf.com>
2026-04-06 12:48:48 -0700

    Downgrade Spring Boot from 4.0.5 to 3.5.12 to align with other MADiE services and fix MongoDB credential handling

--- src/test/java/gov/cms/madie/notification_service/NotificationServiceApplicationTests.java
+++ src/test/java/gov/cms/madie/notification_service/NotificationServiceApplicationTests.java
@@ -2,13 +2,13 @@ package gov.cms.madie.notification_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class NotificationServiceApplicationTests {
 
-  @MockitoBean JwtDecoder jwtDecoder;
+  @MockBean JwtDecoder jwtDecoder;
 
   @Test
   void contextLoads() {}
